1959646d38ba68ab2cacc1c1b4f6defe275806f0
Esteban Margaron <emargaron@microsoft.com>
2024-02-08 15:29:42 -0800

    Finish test

--- SoftwareBitmapMemoryLeak/MainWindow.xaml.cpp
+++ SoftwareBitmapMemoryLeak/MainWindow.xaml.cpp
@@ -7,6 +7,7 @@
 #pragma comment(lib, "shlwapi.lib")
 
 #include <winrt/Windows.Storage.h>
+#include <winrt/Windows.Graphics.Imaging.h>
 
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
@@ -19,6 +20,8 @@ namespace winrt::SoftwareBitmapMemoryLeak::implementation
 
     winrt::fire_and_forget MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
     {
+        DescriptionText().Text(L"Decode test is running...");
+
         WCHAR exePath[MAX_PATH];
         GetModuleFileNameW(NULL, exePath, MAX_PATH);
         PathRemoveFileSpecW(exePath);
@@ -29,7 +32,34 @@ namespace winrt::SoftwareBitmapMemoryLeak::implementation
 
         for (auto file : files)
         {
-            OutputDebugStringW(file.Name().c_str());
+            Windows::Graphics::Imaging::SoftwareBitmap bitmap = nullptr;
+            Windows::Storage::Streams::IRandomAccessStream fileStream = nullptr;
+
+            try
+            {
+                fileStream = co_await file.OpenAsync(Windows::Storage::FileAccessMode::Read);
+                auto decoder = co_await Windows::Graphics::Imaging::BitmapDecoder::CreateAsync(fileStream);
+                if (decoder != nullptr)
+                {
+                    bitmap = co_await decoder.GetSoftwareBitmapAsync();
+                    bitmap.Close();
+                }
+                fileStream.Close();
+            }
+            catch (...)
+            {
+                if (bitmap != nullptr)
+                {
+                    bitmap.Close();
+                }
+
+                if (fileStream != nullptr)
+                {
+                    fileStream.Close();
+                }
+            }
         }
+
+        DescriptionText().Text(L"Decode test is finished. \nClick the button again and you should see the memory permanently increase with each subsequent run.");
     }
 }
